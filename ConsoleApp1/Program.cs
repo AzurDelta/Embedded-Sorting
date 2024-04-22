143c74b7e5ba9c61e6a6cd9466e9bb751ff0ab30
AzurDelta <Rotar.zahar31@gmail.com>
2024-04-22 17:13:10 +0300

    task 2 done

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,25 @@
 ﻿namespace Embedded_Sorting;
-
-class Program
+internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Goodbye, World!");
+        SortByNumber(args);
     }
-}
+
+    static void SortByNumber(string[] args)
+    {
+        List<string> correctOrder = new();
+        var nums = new string[9];
+        for (int i = 0; i < nums.Length; i++)
+            nums[i] = $"{i}";
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < args.Length; j++)
+            {
+                if (args[j].Contains(nums[i])) correctOrder.Add(args[j]);
+            }
+        }
+        
+        correctOrder.ForEach(x => Console.Write($"{x}; "));
+    }
+}   
